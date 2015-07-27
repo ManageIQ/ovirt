98b72730dc28c5b27a72895e59e770e921958144
Brandon Dunne <bdunne@redhat.com>
2015-07-27 18:04:16 -0400

    Clean up event_spec to be consistent with other specs

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,24 +1,24 @@
 describe Ovirt::Event do
   context ".set_event_name" do
-    before :each do
+    before do
       @orig_log, $rhevm_log = $rhevm_log, double("logger")
     end
 
-    after :each do
+    after do
       $rhevm_log = @orig_log
     end
 
     it "sets the name corresponding to a valid code" do
       hash = {:code => 1}
       described_class.send(:set_event_name, hash)
-      expect(hash[:name]).to eq Ovirt::Event::EVENT_CODES[1]
+      expect(hash[:name]).to eq(Ovirt::Event::EVENT_CODES[1])
     end
 
     it "sets 'UNKNOWN' as the name with an invalid code" do
       expect($rhevm_log).to receive :warn
       hash = {:code => -1, :description => "Invalid Code"}
       described_class.send(:set_event_name, hash)
-      expect(hash[:name]).to eq "UNKNOWN"
+      expect(hash[:name]).to eq("UNKNOWN")
     end
   end
 end
